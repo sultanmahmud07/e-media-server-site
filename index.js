cd84680f4b42078905fa228d1b937af20a8b870a
sultanmahmud07 <asadkhan01862@gmail.com>
2022-12-28 13:13:57 +0600

    server connected

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express()
@@ -7,6 +9,16 @@ const app = express()
 //middleware>>>>>>
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.BD_PASS}@cluster0.wtcs29q.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
 
 
 app.get('/', (req, res) => {
