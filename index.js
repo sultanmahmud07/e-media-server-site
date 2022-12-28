370b801fbc6a47a86263d0e0c04695c31ad84b1b
sultanmahmud07 <asadkhan01862@gmail.com>
2022-12-28 12:57:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app = express()
+
+//middleware>>>>>>
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('E-Media.com server is running!')
+})
+
+app.listen(port, () => {
+  console.log(`E-Media.com server is running on port ${port}`)
+})
